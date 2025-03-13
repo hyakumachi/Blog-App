1fc686b5942e30ebafcd8f7fbf507cfbd4ee855b
hyakumachi <pacanza.berns4884@gmail.com>
2025-03-14 00:33:04 +0800

    added middlewares and sqlite database setup

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express')
+const db = require("better-sqlite3")("database.db")
+db.pragma("journal_mode = WAL")
+
+//db setup starts here
+const createTables = db.transaction(() => {
+    db.prepare(`
+        CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username STRING NOT NULL UNIQUE,
+        password STRING NOT NULL
+        )`).run()
+})
+
+createTables()
+//db setup ends here
+
+
 const app = express()
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static("public"))
+
+app.use(function (req, res, next) {
+    res.locals.errors = []
+    next()
+})
 
 app.get("/", (req, res) => {
     res.render("homepage")
@@ -16,26 +38,28 @@ app.get("/login", (req, res) => {
 app.post("/register", (req, res) => {
     const errors = []
 
-    if(typeof req.body.username !== "string" || req.body.username === "") {
-        errors.push("Username must be a non-empty string.")
-    }
-    if(typeof req.body.password !== "string" || req.body.password === "") {
-        errors.push("Password must be a non-empty string.")
-    }
+    if(typeof req.body.username !== "string") req.body.username = "" 
+    if(typeof req.body.password !== "string") eq.body.password = "" 
 
     req.body.username = req.body.username.trim()
 
     if(!req.body.username) errors.push("You must provide a username.")
     if(req.body.username && req.body.username.length < 3) errors.push("Username must be at least 3 characters long.")
-    if(req.body.username && req.body.username.length > 16) errors.push("Username cannot exceed 16 characters.")
+    if(req.body.username && req.body.username.length > 10) errors.push("Username cannot exceed 10 characters.")
     if(req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("Username can only contain alphanumeric characters.")
+    
+    if(!req.body.password) errors.push("You must provide a password.")
+    if(req.body.password && req.body.password.length < 8) errors.push("Password must be at least 8 characters long.")
+    if(req.body.password && req.body.password.length > 21) errors.push("Password cannot exceed 21 characters.")
     
     if(errors.length) {
         return res.render("homepage", { errors })
-    } else {
-        res.send("Thank you for filling out the form.");
-    }
-    
+    } 
+
+    //save the new user into a database
+
+    //log the user in by giving them a cookie
+
 })
 
 app.listen(3000)
